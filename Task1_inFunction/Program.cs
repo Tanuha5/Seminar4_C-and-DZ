ce3227c915c1af23b2dc0acb56ef98293284bdac
Tatiana <Tanuha5@rambler.ru>
2023-02-20 11:52:51 +0300

    Решение через функции

---
+++ Task1_inFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 24: Напишите программу, которая принимает 
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+// Решение через функции
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumElements (int A)
+{
+    int result = 0;
+    for (int i = 1; i <=A; i++)
+    {
+        result +=i; // result = result + i;
+    }
+    return result;
+}
+int num = GetNumber("Введите число А: ");
+System.Console.WriteLine($"Сумма чисел от 1 до {num} = {GetSumElements(num)}");
+
