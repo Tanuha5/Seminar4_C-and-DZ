236e868569023a2e6a332ec581ef571ad0111ce5
Tatiana <Tanuha5@rambler.ru>
2023-02-20 12:34:57 +0300

    Выдает количество цифр в числе

---
+++ Task2_Kolvo/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 26: Напишите программу, которая принимает 
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+string? GetNumber(string text)
+{
+    System.Console.Write(text);
+    return (Console.ReadLine());
+}
+string? num = GetNumber("Введите число А: ");
+int len = num!.Length;
+System.Console.WriteLine($"Количество цифр в числе {num} = {len}.");
