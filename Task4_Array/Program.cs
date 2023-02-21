2d304cb7a3c31ed629bbc2e41ae7f37608d8a018
Tatiana <Tanuha5@rambler.ru>
2023-02-21 13:17:30 +0300

    Второй способ решения массива

---
+++ Task4_Array/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 30: Напишите программу, которая выводит 
+// массив из 8 элементов, заполненный нулями и 
+// единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i=0; i<count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+int[] FillArray()
+{
+    int[] arrN = new int[8];
+    for(int i=0; i<8; i++)
+    {
+        arrN[i] = new Random().Next(0,2);
+    }
+    return arrN;
+}
+int[] myArray = FillArray();
+PrintArray(myArray);
+
+// int[] GetArray()
+// {
+// var arr = new int[8];
+// var rand = new Random();
+// for (int i = 0; i < arr.Length; i++)
+// {
+// arr[i] = rand.Next(0, 2);
+// }
+// return arr;
+// }
+
+// void PrintArray(int[] arr)
+// {
+// System.Console.WriteLine("["+string.Join(", ", arr)+"]");
+// }
+
+// var myArray = GetArray();
+// PrintArray(myArray);
+
+
+
+
+// void GetArray(int[] arr)
+// {
+// var rand = new Random();
+// for (int i = 0; i < arr.Length; i++)
+// {
+// arr[i] = rand.Next(0, 2);
+// }
+// }
+
+// void PrintArray(int[] arr)
+// {
+// System.Console.WriteLine("["+string.Join(", ", arr)+"]");
+// }
+
+// int[] myArray = new int[8];
+// GetArray(myArray);
+// PrintArray(myArray);
